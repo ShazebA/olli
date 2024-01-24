b7ed2055db494f2c939e986a559d74f9270b52c9
ctroubit <ctroubit@uwo.ca>
2024-01-24 15:25:35 -0500

    Adding navbar and renaming components

---
+++ olli/src/HomePage.js
@@ -0,0 +1,28 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+
+function HomePage() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Gallery</Nav.Link>
+            <Nav.Link href="#link">Events</Nav.Link>
+            <Nav.Link href="#link">Contact</Nav.Link>
+            
+            
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default HomePage;
