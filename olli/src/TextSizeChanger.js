841cfed8ae3975aeeb413a2c1414c13e4e5f19ea
EliseDrouillard <edrouil3@uwo.ca>
2024-02-14 13:46:33 -0500

    Added reset button for text size.

--- olli/src/TextSizeChanger.js
+++ olli/src/TextSizeChanger.js
@@ -12,11 +12,19 @@ const TextSizeChanger = () => {
         });
     };
 
+    const resetTextSize = () => {
+        const elementsToResize = document.querySelectorAll('#main-content label, #main-content h1, #main-content h2, #main-content h3, #main-content h4, #main-content h5, #main-content h6, #main-content p');
+        elementsToResize.forEach(element => {
+          element.style.fontSize = '';
+        });
+      };
+
 
     return (
         <div className="text-size-changer">
             <button onClick={() => changeTextSize(1)}>+</button>
             <button onClick={() => changeTextSize(-1)}>-</button>
+            <button onClick={resetTextSize} style={{ width: '80px' }}>Reset</button>
             <label>Text Size</label>
         </div>
     );
