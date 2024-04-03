7db0942465c0a8d5f57ae3007ab0ea40beefd1a2
ctroubit <ctroubit@uwo.ca>
2024-04-03 18:54:13 -0400

    Added sponsors to home page

--- olli/src/HomePage.js
+++ olli/src/HomePage.js
@@ -5,40 +5,33 @@ import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faVolumeUp } from '@fortawesome/free-solid-svg-icons';
 import AccessibilityFooter from './AccessibilityFooter';
+import caregiver from './images/CAREGIVER.png';
+import ofa from './images/OFA.png';
+import rockglen from './images/ROCKGLEN.png';
+import sunset from './images/SUNSET.png';
+
+import './style.css'; // Importing the CSS file for styles
 
 function HomePage() {
-
   let isSpeaking = false;
   const speakText = (text) => {
     const synth = window.speechSynthesis;
+    if (isSpeaking) {
+      synth.cancel();
+      isSpeaking = false; 
+      return; 
+    }
+    const utterThis = new SpeechSynthesisUtterance(text);
+    utterThis.onstart = () => isSpeaking = true;
+    utterThis.onend = () => isSpeaking = false;
+    utterThis.onerror = () => isSpeaking = false;
+    synth.speak(utterThis);
+  };
 
- 
-  if (isSpeaking) {
-    synth.cancel();
-    isSpeaking = false; 
-    return; 
-  }
-
-  
-  const utterThis = new SpeechSynthesisUtterance(text);
-  utterThis.onstart = () => {
-    isSpeaking = true; 
-  };
-  utterThis.onend = () => {
-    isSpeaking = false; 
-  };
-  utterThis.onerror = () => {
-    isSpeaking = false; 
-  };
-  
-  
-  synth.speak(utterThis);
-  };
   function speakSiblingText(event) {
-   
     const cardText = event.target.closest('.card-body').querySelector('.card-text').textContent;
-    const cardtitle = event.target.closest('.card-body').querySelector('.card-title').textContent;
-    speakText(cardtitle+' . '+cardText);
+    const cardTitle = event.target.closest('.card-body').querySelector('.card-title').textContent;
+    speakText(cardTitle + ' . ' + cardText);
   }
 
   return (
@@ -63,12 +56,29 @@ function HomePage() {
                   </Card.Text>
                 </Card.Body>
               </Card>
-              <Card></Card>
+              <Card id='sponsors-card'>
+                <Card.Body>
+                  <h1 className="title-fall">Our Sponsors</h1>
+                  <Row>
+                    <Col xs={6} md={3}>
+                      <a href='https://ontariocaregiver.ca/' target="_blank" rel="noreferrer"><img src={caregiver} alt="Caregiver" className="img-fluid" /></a>
+                    </Col>
+                    <Col xs={6} md={3}>
+                      <a href='https://www.orderalhambra.org/' target="_blank" rel="noreferrer"><img src={ofa} alt="OFA" className="img-fluid"  /></a>
+                    </Col>
+                    <Col xs={6} md={3}>
+                     <a href='https://www.rockglen.com/' target="_blank" rel="noreferrer"><img src={rockglen} alt="Rockglen" className="img-fluid" /></a> 
+                    </Col>
+                    <Col xs={6} md={3}>
+                     <a href='https://sunsetcommunityfoundation.ca/' target="_blank" rel="noreferrer"> <img src={sunset} alt="Sunset" className="img-fluid" /></a> 
+                    </Col>
+                  </Row>
+                </Card.Body>
+              </Card>
             </Col>
           </Row>
         </Container>
       </div>
-
       <AccessibilityFooter />
     </>
   );
